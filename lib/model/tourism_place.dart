d90f56d6a468a429579127a036c58c8902d81580
k.rnandari <k.rnandari@gmail.com>
2024-05-31 15:36:47 +0000

    second

---
+++ lib/model/tourism_place.dart
@@ -0,0 +1,84 @@
+class TourismPlace {
+  final String name;
+  final String location;
+  final String imageAsset;
+  final String imageNetwork;
+  final String imageDetail1;
+  final String imageDetail2;
+  final String imageDetail3;
+  final String description;
+  final String openDays;
+  final String openTime;
+  final String ticketPrice;
+
+   TourismPlace({
+    required this.name,
+    required this.location,
+    required this.imageAsset,
+    required this.imageNetwork,
+    required this.imageDetail1,
+    required this.imageDetail2,
+    required this.imageDetail3,
+    required this.description,
+    required this.openDays,
+    required this.openTime,
+    required this.ticketPrice,
+  });
+}
+
+var tourismPlaceList = [
+  TourismPlace(
+    name: 'Taman Safari Indonesia',
+    location: 'Gn. Princi, Jatiarjo, Kec. Prigen, Pasuruan, Jawa Timur 67157',
+    imageAsset: 'assets/images/tamansafari1.jpg',
+    imageNetwork: 'https://www.agentwisatabromo.com/wp-content/uploads/2016/12/Wisata-Taman-Safari-Prigen-Jawa-Timur.jpg',
+    imageDetail1: 'assets/images/tamansafari4.jpg',
+    imageDetail2: 'assets/images/tamansafari2.jpg',
+    imageDetail3: 'assets/images/tamansafari3.jpg',
+    description: 'Obyek wisata Taman Safari di Kabupaten Pasuruan (TSI II) merupakan salah satu cabang dari Taman Safari di Cisarua Bogor yang terletak di Kecamatan Prigen, di Taman Safari Indonesia II Pasuruan para pengunjung dapat melihat beberapa macam satwa dari beberapa Negara yang hidup seperti habitatnya. Taman safari merupakan obyek wisata nasional yang merupakan Lembaga Konservasi Ex-situ & Pusat Penangkaran Satwa Langka yang mempunyai fungsi Konservasi, Pendidikan, Penelitian, dan Rekreasi.',
+    openDays: 'Setiap Hari',
+    openTime: '08.30 - 15.00',
+    ticketPrice: 'Rp. 235.000 - Rp. 265.000',
+  ),
+    TourismPlace(
+    name: 'Dairyland Farm Theme Park Prigen)',
+    location: 'Jl. Raya Prigen No.8, Plembon, Prigen, Kec. Prigen, Pasuruan, Jawa Timur 67157',
+    imageAsset: 'assets/images/cimory5.jfif',
+    imageNetwork: 'https://akcdn.detik.net.id/visual/2023/06/13/cimory-prigen-1_169.png?w=650',
+    imageDetail1: 'assets/images/cimory2.png',
+    imageDetail2: 'assets/images/cimory3.jpg',
+    imageDetail3: 'assets/images/cimory4.jpg',
+    description: 'Cimory Dairyland Prigen merupakan salah satu cabang pariwisata yang dimiliki PT Cimory dan terletak di Prigen, Pasuruan, Jawa Timur. Tempat ini menawarkan wisata yang edukatif dan menarik dengan tema taman peternakan. Di sini, anak-anak pasti akan senang karena mendapat pengalaman baru dan berbeda seputar peternakan serta pembuatan susu atau yogurt.',
+    openDays: 'Setiap Hari',
+    openTime: '08.30 - 15.00',
+    ticketPrice: 'mulai dari Rp. 25.000',
+  ),
+    TourismPlace(
+    name: 'Air Terjun Putuk Truno',
+    location: 'Jl. Putuk Truno, Prigen, Kec. Prigen, Pasuruan, Jawa Timur 67157',
+    imageAsset: 'assets/images/truno1.jfif',
+    imageNetwork: 'https://petualang.travelingyuk.com/unggah/2019/06/LRM_EXPORT_20190612_175219_5897.jpg',
+    imageDetail1: 'assets/images/truno4.jpg',
+    imageDetail2: 'assets/images/truno3.jpg',
+    imageDetail3: 'assets/images/truno2.jpg',
+    description: 'Wisata Air Terjun Putuk Truno merupakan salah satu destinasi wisata andalan Kabupaten Pasuruan. Berada di kaki Gunung Welirang, Air Terjun Putuk Truno dikelilingi suasana alam yang masih asri. Air terjun yang memiliki ketinggian 45 meter ini dikelilingi tebing dan pepohonan yang hijau.',
+    openDays: 'Setiap Hari',
+    openTime: '24 Jam',
+    ticketPrice: 'Rp. 15.000 - Rp. 20.000',
+  ),
+  TourismPlace(
+    name: 'Saygon Waterpark',
+    location: 'Sengon, Pucangsari, Kec. Purwosari, Pasuruan, Jawa Timur 67162',
+    imageAsset: 'assets/images/saygon1.jpg',
+    imageNetwork: 'https://akcdn.detik.net.id/community/media/visual/2019/05/24/b8a8b04c-b266-42e3-9d2f-6b00073bf82d_169.jpeg?w=700&q=90',
+    imageDetail1: 'assets/images/saygon2.jpg',
+    imageDetail2: 'assets/images/saygon3.jpg',
+    imageDetail3: 'assets/images/saygon4.jpg',
+    description: 'Saygon Waterpark diresmikan pada 8 Juni 2015. Saygon Waterpark dibangun di area yang cukup luas dengan berbagai wahana permainan air dan penginapan. Tak heran, Saygon Waterpark kemudian disebut sebagai kolam renang dan wahana permainan air terbesar di Kabupaten Pasuruan.',
+    openDays: 'Setiap Hari',
+    openTime: '09.00-16.00',
+    ticketPrice: 'mulai dari Rp. 35.000',
+  ),
+];
+
+ 
